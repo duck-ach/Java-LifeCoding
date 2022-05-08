b00825e8abb2cb23a70caa39b77b4bf22071a7c6
duck_ach <djagmlfk@naver.com>
2022-05-08 14:46:03 +0900

    String

---
+++ String/StringApp.java
@@ -0,0 +1,21 @@
+package test_1;
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World"); //String(Character들이 모인 데이타타입)
+		System.out.println('H'); //Character(한 글자를 표현하는 데이터타입)
+		System.out.println("H");
+		
+		System.out.println("Hello"
+				+ "World"); //줄바꿈이 X
+		
+		//new line
+		System.out.println("Hello \nWorld");
+		
+		//escape
+		System.out.println("Hello \"World\"");//Hello "World"
+	}
+
+}
