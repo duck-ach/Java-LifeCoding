bf4eaa7d502a1945604f1dd7006121aa43dff45b
duck-ach <101095613+duck-ach@users.noreply.github.com>
2022-05-18 21:20:57 +0900

    비교연산자

---
+++ JAVA-flow-control/ComparisonOperatorApp.java
@@ -0,0 +1,11 @@
+
+public class ComparisonOperatorApp {
+	 public static void main(String[] args) {
+		 
+		 System.out.println(1 > 1); //false
+		 System.out.println(1 == 1); //true
+		 System.out.println(1 < 1); //false
+		 System.out.println(1 >= 1); //true
+	 }
+	 //비교연산자 == 앞에있는것과 뒤에있는것을 비교해서 ture나 false를 추출해낸다.
+}
