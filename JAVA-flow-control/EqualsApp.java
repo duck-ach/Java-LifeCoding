d1d92448e8c53e77f3de0becb3d4f78be412989e
duck-ach <101095613+duck-ach@users.noreply.github.com>
2022-05-19 14:29:51 +0900

    Equals App

---
+++ JAVA-flow-control/EqualsApp.java
@@ -0,0 +1,14 @@
+
+public class EqualsApp {
+
+	public static void main(String[] args) {
+		
+		String o1 = "java";
+		String o2 = new String("java");
+		
+		// o1 == o2  false
+		// o1.equals(o2) true
+
+	}
+
+}
