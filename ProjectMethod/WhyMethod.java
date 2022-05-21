43bb5a093a95cf4e84f60511368cc989809e4b1f
duck_ach <djagmlfk@naver.com>
2022-05-21 11:56:53 +0900

    MethodBasic

---
+++ ProjectMethod/WhyMethod.java
@@ -0,0 +1,22 @@
+
+public class WhyMethod {
+
+	
+	public static void main(String[] args) {
+		
+		//10000000
+		PrintToTimesA();
+		//10000000
+		PrintToTimesA();
+		//10000000
+		PrintToTimesA();
+
+	}
+
+	public static void PrintToTimesA() {
+		System.out.println("-");
+		System.out.println("A");
+		System.out.println("A");
+	}
+
+}
