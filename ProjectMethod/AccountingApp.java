be89c0ddaa84fb2103c35fa624badbbb7b75d849
duck_ach <djagmlfk@naver.com>
2022-05-29 19:05:49 +0900

    Use class

--- ProjectMethod/AccountingApp.java
+++ ProjectMethod/AccountingApp.java
@@ -1,24 +1,22 @@
-
-public class AccountingApp {
-	//공급가액
-	public static double valueOfSupply = 10000.0;		
-	//부가가치세
+class Accounting {
+	public static double valueOfSupply;	
 	public static double vatRate = 0.1;
-	
 	public static double getVAT() {
 		return valueOfSupply * vatRate;
 	}
-	
 	public static double getTotal() {
 		return valueOfSupply + getVAT();
 	}
 	
+}
+public class AccountingApp {
+	
 	public static void main(String[] args) {
+		Accounting.valueOfSupply = 10000.0;	
+		System.out.println("Value of supply : " + Accounting.valueOfSupply);
+		System.out.println("VAT : " + Accounting.getVAT());
+		System.out.println("Total : " + Accounting.getTotal());
 		
-		System.out.println("Value of supply : " + valueOfSupply);
-		System.out.println("VAT : " + getVAT());
-		System.out.println("Total : " + getTotal());
-
 	}
 
 }
