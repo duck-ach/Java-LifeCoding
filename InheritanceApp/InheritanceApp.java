86afebfabce751f9a55110de81d68684d46535db
duck_ach <djagmlfk@naver.com>
2022-06-01 15:46:27 +0900

    this, super

--- InheritanceApp/InheritanceApp.java
+++ InheritanceApp/InheritanceApp.java
@@ -6,8 +6,8 @@ class Cal{
 	//자바는 같은 이름의 메소드를 여러 개 과적할 수 있음.
 	//오버로딩 자체는 상속과는 큰 상관 X
 	public int sum(int v1, int v2, int v3) {
-		return v1+v2+v3;
-	}
+		return this.sum(v1,v2)+v3;
+	} //sum만 붙여도 상관없지만 명시적으로 this를 붙여줘야한다. (자기자신) 
 }
 class Cal3 extends Cal{
 	public int minus(int v1, int v2) {
@@ -16,8 +16,9 @@ class Cal3 extends Cal{
 	// Overriding 부모 클래스가 가지고 있는 메소드 재정의(덮어쓰기)
 	public int sum(int v1, int v2) {
 		System.out.println("Cal3!!");
-		return v1+v2;
+		return super.sum(v1,v2);
 }
+	//super : 부모, this : 자기자신 
 }
 public class InheritanceApp {
 
